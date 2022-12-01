ab2b01b51e2d83baab8e353f644f5d56fd50aa33
ganeshd0909 <ganeshd9900W@gmail.com>
2022-12-01 17:40:29 +0530

    srcond commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit , NgZone } from '@angular/core';
+import * as am4core from "@amcharts/amcharts4/core";
+import * as am4charts from "@amcharts/amcharts4/charts";
+import am4themes_animated from "@amcharts/amcharts4/themes/animated";
+
+am4core.useTheme(am4themes_animated);
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Dashboard';
+  width: any;
+  subPleas:any;
+
+  // constructor() {  }
+  // ngOnInit() {
+  //   this.burgerMenuClick();
+  // }
+  // burgerMenuClick(){
+
+  // }
+
+  ngOnInit(){
+    this.width= 45 ;
+  }
+  // private chart: am4charts.XYChart;
+
+  constructor(private zone: NgZone){};
+
+  ngAfterViewInit() {
+    this.zone.runOutsideAngular(() => {
+      let chart = am4core.create("line-chart", am4charts.XYChart);
+      let title = chart.titles.create();
+      title.text = "Product Sales by area";
+
+      let data = [
+        { "area": "Florida", "computers":20, "cars": "",}
+      ]
+    })
+  }
+
+  ngOnDestroy(){
+
+  }
+
+  ngCircleOption(){
+    this.width=22;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { Chart } from 'chart.js';
 
 @NgModule({
   declarations: [
@@ -10,9 +14,43 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatSlideToggleModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    NgCircleProgressModule.forRoot({
+      "radius": 30,
+      "space": -10,
+      "unitsColor": "#4882c2" ,
+      "outerStrokeGradient": false,
+      "outerStrokeWidth": 15,
+      "outerStrokeColor": "#7AEE76",
+      "outerStrokeGradientStopColor": "#53a9ff",
+      "innerStrokeColor": "",
+      "innerStrokeWidth": 10,
+      "title": "",
+      "animateTitle": false,
+      "animationDuration": 1000,
+      "showUnits": false,
+      "showBackground": false,
+      "showImage": true,
+      "clockwise": true,
+      "startFromZero": false,
+      "lazy": true,
+      // "subtitleFormat": (percent:number): string =>{
+      //   if(percent < 25){
+      //     this.ngCircleOption.outerStrokeColor = "red";
+      //   }
+      //   else{
+      //     this.ngCircleOption.outerStrokeColor = "green";
+      //   }
+      //   return '';
+      // }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
